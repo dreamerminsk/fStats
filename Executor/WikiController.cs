486e1033eb49268319af8a9f5aa1bec52ae462d3
KCH <tutmirage@gmail.com>
2019-05-11 20:43:16 +0300

    WikiController

---
+++ Executor/WikiController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace fStats.Executor
+{    
+
+    public class WikiController
+    {
+
+        private static readonly HttpClient HttpClient;
+
+        static WikiController()
+        {
+            HttpClient = new HttpClient();
+        }
+
+        public static async Task<string> GetPageAsync(Uri uri)
+        {
+            try
+            {
+                string responseBody = await HttpClient.GetStringAsync(uri);
+
+                return responseBody;
+            }
+            catch (HttpRequestException e)
+            {
+                Console.WriteLine("\nException Caught!");
+                Console.WriteLine("Message :{0} ", e.Message);
+                throw e;
+            }
+        }
+
+    }
+
+}
